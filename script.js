757df95992299d2ac13d3e7e481ccb0f80cfe88e
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-04-27 13:34:31 -0400

    clean up css, add logic to display nums, make buttons kind of work

--- script.js
+++ script.js
@@ -1,47 +1,73 @@
-let numbers = document.querySelectorAll('.number');
-let operators = document.querySelectorAll('.operator');
-let equals = document.querySelector('#enter')
-let operator;
+// create functions for adding, subtracting, multiplying, and dividing
+// the calculator operation will consist of a number, an operator, and another number. Create three variables for each of the parts of the calculators operation
+// create a new function 'operate' that takes an operator and 2 numbers and then calls one of the operation functions above
+// create a function that populates the display when you click the number buttons, you should be storing the display value in a variable somewhere
 
-numbers.forEach((number)=>{
-    number.addEventListener('click', ()=>{
-        return number.textContent
-    })
-})
-
-operators.forEach(function(operator){
-    operator.addEventListener('click', ()=>{
-        return operator
-    })
-})
-
-function add(number, operator){
-    return num1 + num2;
+function add(a, b) {
+    return a + b;
 }
-function subtract(num1, num2, operator){
-    return num1 - num2;
+function subtract(a, b) {
+    return a - b;
 }
-function multiply(num1, num2, operator){
-    return num1 * num2;
+function multiply(a, b) {
+    return a * b;
 }
-function divide(num1, num2, operator){
-    return num1 / num2;
+function divide(a, b) {
+    return a / b;
 }
-function operate(number, num2, operator){
-    if(operator === '+'){
-        console.log(num1 + num2)
-        alert(add(number))
-        return add(number, number);
-    }
-    else if(operator === '-'){
-        return subtract(num1, num2);
-    }
-    else if(operator === '*'){
-        return multiply(num1, num2);
-    }
-    else if(operator === '/'){
-        return divide(num1, num2);
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
     }
 }
 
-equals.addEventListener('click', operate());
+let displayValue = "";
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let result = "";
+let display = document.querySelector(".calc-screen");
+let numberButtons = document.querySelectorAll(".number");
+let operatorButtons = document.querySelectorAll(".operator");
+let equalsButton = document.querySelector(".equals");
+let clearButton = document.querySelector(".clear");
+
+clearButton.addEventListener("click", () => {
+    displayValue = "";
+    display.textContent = displayValue;
+});
+
+// if number is clicked, assign to the first number variable
+// if operator is clicked, assign to the operator variable
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        displayValue += button.textContent;
+        display.textContent = displayValue;
+        console.log(displayValue.length);
+        if(displayValue.length >= 9){
+            display.textContent = "Error";
+        }
+    });
+    
+});
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        firstNumber = displayValue;
+        displayValue = "";
+        operator = button.textContent;
+    });
+}
+);
+
+equalsButton.addEventListener("click", () => {
+    secondNumber = displayValue;
+    displayValue = "";
+    display.textContent = operate(operator, firstNumber, secondNumber);
+});
